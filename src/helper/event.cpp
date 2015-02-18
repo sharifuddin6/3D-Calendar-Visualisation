1affb7d1885883903e8a9a71504527760b76d462
Sharif Uddin <sharifuddin6@outlook.com>
2015-02-18 00:33:28 +0000

    Fixed bug: Segmentation error during parsing large ics data file.

--- src/helper/event.cpp
+++ src/helper/event.cpp
@@ -1,6 +1,8 @@
 #include "event.h"
+#include "string"
 #include "string.h"
 #include "stdio.h"
+using namespace std;
 
 // constructor
 Event::Event() { }
@@ -36,38 +38,64 @@ char* Event::getLocation() {
 
 // setters
 void Event::setSubject(const char* newSubject) {
-
+  string tmp = string(newSubject);
+  subject = new char[tmp.length() + 1];
+  strcpy(subject, tmp.c_str());
+  tmp.clear();
 }
 
 void Event::setStartDate(const char* newStartDate) {
-
+  string tmp = string(newStartDate);
+  startDT = new char[tmp.length() + 1];
+  strcpy(startDT, tmp.c_str());
+  tmp.clear();
 }
 
 void Event::setStartTime(const char* newStartTime) {
-
+  string tmp = string(newStartTime);
+  startTM = new char[tmp.length() + 1];
+  strcpy(startTM, tmp.c_str());
+  tmp.clear();
 }
 
 void Event::setStartDateTime(const char* newStartDateTime) {
-
+//  string tmp = string(newDescription);
+//  description = new char[tmp.length() + 1];
+//  strcpy(description, tmp.c_str());
+//  tmp.clear();
 }
 
 void Event::setEndDate(const char* newEndDate) {
-
+  string tmp = string(newEndDate);
+  endDT = new char[tmp.length() + 1];
+  strcpy(description, tmp.c_str());
+  tmp.clear();
 }
 
 void Event::setEndTime(const char* newEndTime) {
-
+  string tmp = string(newEndTime);
+  endTM = new char[tmp.length() + 1];
+  strcpy(endTM, tmp.c_str());
+  tmp.clear();
 }
 
 void Event::setEndDateTime(const char* newEndDateTime) {
-
+//  string tmp = string(newEndDateTime);
+//  endDT = new char[tmp.length() + 1];
+//  strcpy(endDT, tmp.c_str());
+//  tmp.clear();
 }
 
 void Event::setDescription(const char* newDescription) {
-  description = new char[sizeof(newDescription)+1];
-  strcpy(description, newDescription);
+  string tmp = string(newDescription);
+  description = new char[tmp.length() + 1];
+  strcpy(description, tmp.c_str());
+  tmp.clear();
 }
 
 void Event::setLocation(const char* newLocation) {
-
+  string tmp = string(newLocation);
+  location = new char[tmp.length() + 1];
+  strcpy(location, tmp.c_str());
+  tmp.clear();
 }
